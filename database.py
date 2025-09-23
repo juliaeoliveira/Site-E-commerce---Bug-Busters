77910e778d52551d528c4fb68a78596e47b5e86b
Gustavo Albuquerque <gustavo.albuquerquee1@gmail.com>
2025-09-23 18:55:37 -0300

    feat: Correções de relacionamentos

--- database.py
+++ database.py
@@ -7,18 +7,21 @@ from sqlalchemy.orm import sessionmaker,declarative_base, relationship
 from sqlalchemy import Column,Integer,String,Float,Boolean,DateTime
 #Importação date
 from datetime import datetime
+import pytz
 
 
 engine=create_engine("sqlite:///loja.db")
 SessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
 
+
+fuso = pytz.timezone('America/Sao_Paulo')
 class Cliente(Base):
     __tablename__="cliente"
-    cpf = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome_cliente = Column(String, index=True) 
-    data_nascimento= Column(DateTime, default=datetime.utcnow)
-    data_cadastro = Column(DateTime, default=datetime.utcnow)
+    data_nascimento = Column(DateTime, nullable=False)
+    data_cadastro = Column(DateTime, default=lambda: datetime.now(fuso))
     email = Column(String, index=True)
     telefone = Column(String)
     
@@ -28,11 +31,13 @@ class Cliente(Base):
     complemento = Column(String, nullable=True)
     bairro = Column(String)
     cidade = Column(String)
-    estado = Column(String(2))  # Ex: "SP", "RJ"
-    cep = Column(String(10))    # Ex: "12345-678"
-    pais = Column(String, default="Brasil")
-    #relacionamento com a tabela pedidos
+    estado = Column(String(2))  #"SP", "RJ"
+    cep = Column(String(10))    #"12345-678"
+
+    #relacionamento com a tabela pagamento e Pedido
+    pagamentos = relationship("Pagamento", back_populates="cliente")
     pedidos = relationship("Pedido", back_populates="cliente")
+    
 
 class Loja(Base):
     __tablename__="loja"
@@ -45,35 +50,44 @@ class Loja(Base):
     complemento = Column(String, nullable=True)
     bairro = Column(String)
     cidade = Column(String)
-    estado = Column(String(2))  # Ex: "SP", "RJ"
-    cep = Column(String(10))    # Ex: "12345-678"
-    pais = Column(String, default="Brasil")
+    estado = Column(String(2))  #"SP", "RJ"
+    cep = Column(String(10))    #"12345-678"
     
     telefone = Column(String)
     email = Column(String)
 
+    # relacionamento com a tabela pagamento
+    pagamentos = relationship("Pagamento", back_populates="loja")
+
 class Pagamento(Base):
     __tablename__="pagamento"
     id = Column(Integer,primary_key=True) 
-    data_pagamento = Column(DateTime, default=datetime.utcnow)
+    data_pagamento = Column(DateTime, default=lambda: datetime.now(fuso))
     valor = Column(Float)
     metodo_pagamento = Column(String)
     status = Column(Boolean, default=True)
+
     #chave estrangeira
-    produto_id = Column(Integer, ForeignKey('produtos.id'), nullable=False)
-    #relacionamento com a tabela Produto
-    produto = relationship("Produtos", back_populates="pagamento")
+    id_cliente = Column(Integer, ForeignKey('cliente.id'), nullable=False)
+    id_pedido = Column(Integer, ForeignKey('pedido.id'), unique=True)
+    cnpj_loja = Column(Integer, ForeignKey('loja.cnpj'))
+    
+    #relacionamento com a tabela Clente 
+    cliente = relationship("Cliente", back_populates="pagamentos")
+    pedido = relationship("Pedido", back_populates="pagamento")
+    loja = relationship("Loja", back_populates="pagamentos")
 
 class Pedido(Base):
     __tablename__="pedido"
     id = Column(Integer,primary_key=True)
-    data_pedido = Column(DateTime, default=datetime.utcnow)
+    data_pedido = Column(DateTime, default=lambda: datetime.now(fuso))
     valor_total = Column(Float)
     status = Column(Boolean, default=True)
     #chave estrangeira
-    cliente_id = Column(Integer, ForeignKey('cliente.cpf'), nullable=False)
+    id_cliente = Column(Integer, ForeignKey('cliente.id'), nullable=False)
+    pagamento = relationship("Pagamento", uselist=False, back_populates="pedido")
     #relacionamento com a tabela cliente
-    cliente = relationship("Cliente", back_populates="cliente")
+    cliente = relationship("Cliente", back_populates="pedidos")
 
 class Produtos(Base):
     __tablename__="produtos"
@@ -82,12 +96,10 @@ class Produtos(Base):
     preco = Column(Float)
     categoria = Column(String,index=True)
     estoque = Column(Integer, default=0)
-    data_cadastro = Column(DateTime, default=datetime.utcnow)
+    data_cadastro = Column(DateTime, default=lambda: datetime.now(fuso))
     imagem_URL = Column(String)
     status = Column(Boolean, default=True)
     descricao=Column(String)
-    #relacionamento com a tabela pagamento
-    pagamentos = relationship("Pagamento", back_populates="produto")
 
 #Criar todas tabela e o banco de dados no sqlite
 Base.metadata.create_all(bind=engine)
