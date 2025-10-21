0d841eb65a44778290b33ecc2a357fecc5a4caac
Gustavo Albuquerque <gustavo.albuquerquee1@gmail.com>
2025-10-20 22:27:25 -0300

    rotas login integradas com o front

--- controller/usuario_controller.py
+++ controller/usuario_controller.py
@@ -7,7 +7,12 @@ from controller.usuario_autenticacao import ServicosUsuario, verificar_hash_senh
 from models import Usuario_Model
 from schemas import Usuario , Endereco
 
-templates=Jinja2Templates(directory="templates")
+from datetime import datetime
+import pytz
+
+fuso = pytz.timezone('America/Sao_Paulo')
+
+templates=Jinja2Templates(directory="view/templates")
 caminho_prefixo_usuario = APIRouter(prefix='/usuario')
 
 
@@ -22,12 +27,10 @@ def pagina_cadastro(request:Request):
 def cadastrar_usuario( request:Request,
     nome_cliente: str = Form(...),
     data_nascimento: str = Form(...),
-    data_cadastro: str = Form(...),
     email: str = Form(...),
     telefone: str = Form(...),
     nome_usuario: str = Form(...),
     senha: str = Form(...),
-    tipo: str = Form(...),
 
     rua: str = Form(...),
     numero: str = Form(...),
@@ -39,13 +42,22 @@ def cadastrar_usuario( request:Request,
 
     db: Session = Depends(get_db)
 ):
-    usuario=db.query(Usuario_Model).filter(Usuario_Model.nome_usuario==nome_usuario).first()
-    if usuario:
-        return {"mensagem":"Nome de usuário já cadastrado!"}
+    
+    email = email.strip().lower()
+    
+    #define o tipo com base no domínio do e-mail
+    if email.endswith("@adm_sobveu.com"):
+        tipo = "adm"
+    else:
+        tipo = "cliente"
+
+    email=db.query(Usuario_Model).filter(Usuario_Model.email==email).first()
+    if email:
+        return {"mensagem":"Email já cadastrado!"}
     novo_usuario=Usuario(
         nome_cliente=nome_cliente,
         data_nascimento=data_nascimento,
-        data_cadastro=data_cadastro,
+        data_cadastro=datetime.now(fuso),
         email=email,
         telefone=telefone,
         nome_usuario=nome_usuario,
@@ -70,38 +82,6 @@ def cadastrar_usuario( request:Request,
         status_code=status.HTTP_201_CREATED
     ) , RedirectResponse(url="/",status_code=303)
 
-
-
-
-
-
-
-
-# @caminho_prefixo_usuario.post('/registrar')
-# def usuario_registrar(
-#     usuario : Usuario,
-#     endereco : Endereco,
-#     db_session: Session = Depends(get_db_session), #cria uma dependência que fornece uma sessão para a função, garantindo que ela seja aberta e fechada corretamente
-# ):
-#     #verificar se o nome de usuário já existe
-#     usuarios_existente = db_session.query(Usuario_Model).filter(Usuario_Model.nome_usuario == usuario.nome_usuario).first()
-#     if usuarios_existente:
-#         raise HTTPException(status_code=400, detail="Usuário já existe.")
-    
-#     #verificar se o email já existe
-#     if usuario.email:
-#         emails_existente = db_session.query(Usuario_Model).filter(Usuario_Model.email == usuario.email).first()
-#         if emails_existente:
-#             raise HTTPException(status_code=400, detail="Email já está em uso.")
-    
-#    #se passar por todas as validações add usuario
-#     su = ServicosUsuario(db_session=db_session)
-#     su.registrar_usuario(usuario=usuario,endereco=endereco)
-#     return JSONResponse(
-#         content={'msg': 'sucesso'},
-#         status_code=status.HTTP_201_CREATED
-#     )
-
 #rota login usuário
 @caminho_prefixo_usuario.get("/login",response_class=HTMLResponse)
 def home(request:Request):
@@ -109,14 +89,14 @@ def home(request:Request):
                             {"request":request})
 #post login do usuário
 @caminho_prefixo_usuario.post("/login")
-def login(request:Request, nome_usuario:str=Form(...),
+def login(request:Request, email:str=Form(...),
         senha:str=Form(...), db:Session=Depends(get_db)
 ):
-    usuario=db.query(Usuario_Model).filter(Usuario_Model.nome_usuario==nome_usuario).first()
+    usuario=db.query(Usuario_Model).filter(Usuario_Model.email==email).first()
     if not usuario or not verificar_hash_senha(senha,
                                                usuario.senha):
         return {"mensagem":"Credenciais inválidas"}
-    token=criar_token({"sub":usuario.nome_usuario})
+    token=criar_token({"sub":usuario.email})
     response=RedirectResponse(url="/usuario/dashboard",status_code=303)
     response.set_cookie(key="token",value=token,
                         httponly=True)
@@ -130,33 +110,3 @@ def dashboard(request:Request):
         return RedirectResponse(url="/",status_code=303)
     return templates.TemplateResponse("dashboard.html",
                     {"request":request})
-
-
-
-
-
-
-
-
-
-
-
-
-# @caminho_prefixo_usuario.post('/login')
-# def usuario_login(
-#     usuario_login: UsuarioLogin,
-#     db_session: Session = Depends(get_db_session),
-# ):
-#     su = ServicosUsuario(db_session=db_session)
-
-#     dados_autenticacao = su.usuario_login(usuario=usuario_login)
-#     return JSONResponse(
-#         content=dados_autenticacao,
-#         status_code=status.HTTP_200_OK
-#         )
-
-
-
-############################################################
-
-#CRUD de usuarios
